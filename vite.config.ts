34bac06dd5035fe98cc93a3de43e441a73d8b86d
Bruno Carvalho <bruno@brunocarvalho.me>
2025-11-25 09:47:27 -0300

    Fix tailwind version to use v4 and add theme variation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="vitest" />
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() as any],
+  plugins: [react() as any, tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
