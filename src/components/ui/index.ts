ba3b7cee57f7f839897389a82f96f0729941f216
Bruno Carvalho <bruno@brunocarvalho.me>
2025-11-25 18:43:11 -0300

    Create table component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,11 @@
 export { Button, LoadingButton } from './button';
 export { Input } from './input';
 export { Label } from './label';
+export {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './table';
