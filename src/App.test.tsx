c8a5615f01d0db83c77504291f4b7b5cdacbc428
Bruno Carvalho <bruno@brunocarvalho.me>
2025-11-24 21:27:20 -0300

    Mock useAuth to test authenticated and unauthenticated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
 import { render, screen } from '@testing-library/react';
+import { Mock, vi } from 'vitest';
 
 import App from './App';
+import { useAuth } from './hooks/useAuth';
+
+vi.mock('./hooks/useAuth');
+
+const mockedUseAuth = useAuth as Mock;
 
 describe('App', () => {
-  it('renders the app', () => {
+  beforeEach(() => {
+    mockedUseAuth.mockReset();
+  });
+
+  it('renders the dashboard when user is authenticated', () => {
+    mockedUseAuth.mockReturnValue(true);
+
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', { name: 'Dashboard' }),
+    ).toBeInTheDocument();
+  });
+
+  it('renders the login page when user is not authenticated', () => {
+    mockedUseAuth.mockReturnValue(false);
+
     render(<App />);
 
     expect(screen.getByRole('heading', { name: 'Login' })).toBeInTheDocument();
