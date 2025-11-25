cbeddf7fb509b01a703ff921ab26c3496809701f
Bruno Carvalho <bruno@brunocarvalho.me>
2025-11-25 10:24:35 -0300

    Add loading state for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 
 import { cn } from '../../lib/utils';
+import { Loader2 } from 'lucide-react';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'default' | 'destructive' | 'outline' | 'ghost' | 'link';
+  loading?: boolean;
   size?: 'default' | 'sm' | 'lg' | 'icon';
+  variant?: 'default' | 'destructive' | 'outline' | 'ghost' | 'link';
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant = 'default', size = 'default', ...props }, ref) => {
+  (
+    {
+      children,
+      className,
+      disabled,
+      loading,
+      variant = 'default',
+      size = 'default',
+      ...props
+    },
+    ref,
+  ) => {
     const variants = {
       default: 'bg-primary text-primary-foreground hover:bg-primary/90',
       destructive:
@@ -30,14 +43,22 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         className={cn(
-          'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+          'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors cursor-pointer relative focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-70',
           variants[variant],
           sizes[size],
           className,
         )}
         ref={ref}
+        disabled={loading || disabled}
         {...props}
-      />
+      >
+        <span className={cn({ 'opacity-0': loading })}>{children}</span>
+        {loading && (
+          <span className="absolute inset-0 flex items-center justify-center">
+            <Loader2 className="h-4 w-4 animate-spin" />
+          </span>
+        )}
+      </button>
     );
   },
 );
